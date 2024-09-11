b8f6b0f19350d5da31e235dbd1743d3b9ec39479
juanrosendoo <juanito.rosendo@hotmail.com>
2024-09-11 10:05:39 -0300

    :alien

---
+++ Testes/Teste.java
@@ -0,0 +1,47 @@
+import java.io.File;
+import java.io.IOException;
+import java.util.Scanner;
+import javax.sound.sampled.*;
+
+public class Teste{
+
+    private Clip clip;
+
+    Scanner sc = new Scanner(System.in);
+
+    public void play() throws LineUnavailableException{
+
+        this.clip.start();
+
+        @SuppressWarnings("unused")
+        String response = sc.next();
+        
+    }
+
+    public void pause() throws LineUnavailableException{
+
+        this.clip.stop();
+
+        @SuppressWarnings("unused")
+        String response = sc.next();
+
+    }
+
+    public void reset() throws LineUnavailableException{
+
+        this.clip.setMicrosecondPosition(0);
+        
+        @SuppressWarnings("unused")
+        String response = sc.next();
+
+    }
+
+    public void setClip(String nomeDoAudio) throws UnsupportedAudioFileException, IOException, LineUnavailableException{
+
+        File file = new File("./TopGear/game_sounds/" + nomeDoAudio + ".wav");
+        AudioInputStream audioStream = AudioSystem.getAudioInputStream(file);
+        this.clip = AudioSystem.getClip();
+        this.clip.open(audioStream);
+
+    }
+}
